a95ae52a55335bfc279f351843995597540c590a
asenanov7 <asenanov7@gmail.com>
2024-09-06 21:53:52 +0300

    training

--- lesson3/src/main/java/com/example/lesson3/Lesson3Activity.kt
+++ lesson3/src/main/java/com/example/lesson3/Lesson3Activity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.lesson3.ui.theme.JetpackComposeTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class Lesson3Activity : ComponentActivity() {
 
@@ -19,30 +24,38 @@ class Lesson3Activity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            JetpackComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            AlertDialogSample()
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Preview
+    @Composable
+    fun OutlinedButtonSample() {
+        OutlinedButton(shape = RoundedCornerShape(size = 4.dp), onClick = { /* Do something! */ }) { Text("Outlined Button") }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackComposeTheme {
-        Greeting("Android")
+    @Preview
+    @Composable
+    fun InputLayout() {
+        val text = rememberSaveable { mutableStateOf("") }
+
+        TextField(
+            value = text.value,
+            onValueChange = { text.value = it },
+            label = { Text(text = "asd") }
+
+        )
+    }
+
+    @Preview
+    @Composable
+    fun AlertDialogSample() {
+        AlertDialog(
+            title = { Text(text = "Are you sure?", fontSize = 14.sp) },
+            text = { Text(text = "Do you want to delete this file?") },
+            onDismissRequest = { /*TODO*/ },
+            confirmButton = { Text(text = "Yes", Modifier.padding(8.dp)) },
+            dismissButton = { Text(text = "No", Modifier.padding(8.dp)) }
+        )
     }
 }
